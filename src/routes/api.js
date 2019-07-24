10bc4cd05b78976ac85d5108130ceb220aa2398c
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-07-24 14:35:41 +0200

    New Route '/add' for create todo

--- src/routes/api.js
+++ src/routes/api.js
@@ -12,10 +12,11 @@ const router = Router();
  * Controllers imports
  */
 const read = require('../controllers/Todo/Read');
-
+const create = require('../controllers/Todo/Create');
 /**
  * Routes
  */
 router.get('/all', read);
+router.post('/add', create);
 
 module.exports = router;
--- src/routes/api.test.js
+++ src/routes/api.test.js
@@ -18,4 +18,36 @@ describe('[Routes] - API', () => {
                 .expect(200, done);
         });
     });
+
+    describe('Add', () => {
+        it('should return status 200', (done) => {
+            // Arrange
+
+            // Act
+
+            // Assert
+            request(app)
+                .post('/api/add')
+                .send({
+                    task: 'devAgain'
+                })
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+    describe('Add', () => {
+        it('send without body should return status 400', (done) => {
+            // Arrange
+
+            // Act
+
+            // Assert
+            request(app)
+                .post('/api/add')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(400, done);
+        });
+    });
 });
