2cdfaf1c470d091dc71a8e6aab15f7bc038c5771
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-07-24 11:07:16 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const apiRouter = require('./routes/api');
+
+const app = express();
+
+
+
+app.use(bodyParser.json());
+
+
+app.use('/api', apiRouter);
+
+
+
+export default app;
