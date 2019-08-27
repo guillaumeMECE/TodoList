1bb16779becaefe367f6c1202c258337af9f5965
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-08-27 16:03:54 +0200

    Add register and login handler in controllers/index

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -7,4 +7,8 @@ module.exports = {
     Delete: require('./todo/Delete'),
     Read: require('./todo/Read'),
     Update: require('./todo/Update'),
+
+    // auth handlers
+    RegisterUser: require('./auth/RegisterUser'),
+    LoginUser: require('./auth/LoginUser'),
 };
